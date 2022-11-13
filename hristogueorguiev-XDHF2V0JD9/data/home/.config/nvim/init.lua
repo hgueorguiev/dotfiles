7d0db7f89bcfb3972efec5e76fbde4b925255596
Hristo I. Gueorguiev <hgueorguiev@gocatalant.com>
2022-11-13 18:53:51 -0500

    Start nvim configs to fix qf list nonsense

--- hristogueorguiev-XDHF2V0JD9/data/home/.config/nvim/init.lua
+++ hristogueorguiev-XDHF2V0JD9/data/home/.config/nvim/init.lua
@@ -45,6 +45,15 @@ vim.g.mapleader = " "
 --------------------------------------------------------------------------------
 -- Auto commands
 --------------------------------------------------------------------------------
+-- Ex
+-- vim.api.nvim_create_autocmd({ event = "FileType",
+--                               group = "MyGroupName",
+--                               pattern = "*",
+--                               callback = function
+--                                print("Filetype autocmd")
+--                               end,
+--                               once = true})
+--------------------------------------------------------------------------------
 vim.api.nvim_create_autocmd( -- Enter insert mode when entering a Term buffer
 	{
 		"BufEnter",
@@ -56,6 +65,56 @@ vim.api.nvim_create_autocmd( -- Enter insert mode when entering a Term buffer
 )
 
 --------------------------------------------------------------------------------
+-- Better QuickFix plugin proto TODO:Move to a plugin, refactor, fix sign jitter
+--------------------------------------------------------------------------------
+-- Why would parsing edited qfix list not be default, I would really like to hear the reasoning here
+vim.go.errorformat = vim.go.errorformat .. ",%f|%l col %c|%m"
+
+vim.keymap.set("n", "<LEADER>qo", ":copen<CR>", { silent = true })
+
+function better_qf()
+	vim.cmd("sign define QFmatch text=")
+	local cur_buf = vim.api.nvim_get_current_buf()
+	vim.keymap.set(
+		"n",
+		"J",
+		":cnext<CR>zz:QFUnMarkAll<CR>:QFMarkCurrentLine<CR><C-w>w",
+		{ remap = false, buffer = cur_buf }
+	)
+	vim.keymap.set(
+		"n",
+		"K",
+		":cprev<CR>zz:QFUnMarkAll<CR>:QFMarkCurrentLine<CR><C-w>w",
+		{ remap = false, buffer = cur_buf }
+	)
+	vim.keymap.set("n", "H", ":colder<CR>", { remap = false, buffer = cur_buf })
+	vim.keymap.set("n", "L", ":cnewer<CR>", { remap = false, buffer = cur_buf })
+	vim.keymap.set("n", "i", ":set modifiable<CR>", { remap = false, buffer = cur_buf })
+	vim.keymap.set("n", "<C-s>", ":cgetbuffer<CR>:cclose<CR>:copen<CR>", { remap = false, buffer = cur_buf })
+end
+
+vim.api.nvim_create_autocmd( -- Enter insert mode when entering a Term buffer
+	{
+		"FileType",
+	},
+	{
+		pattern = { "qf" },
+		callback = better_qf,
+	}
+)
+
+vim.api.nvim_create_user_command(
+	"QFMarkCurrentLine",
+	'exe "sign place 1 name=QFmatch group=QFMatches line="..line(".")',
+	{}
+)
+
+vim.api.nvim_create_user_command("QFUnMarkAll", "sign unplace * group=QFMatches", {})
+--------------------------------------------------------------------------------
+-- END Better QuickFix plugin proto
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
 -- Custom commands
 --------------------------------------------------------------------------------
 vim.api.nvim_create_user_command("ReloadConfig", "source $MYVIMRC", {})
@@ -63,6 +122,15 @@ vim.api.nvim_create_user_command("ToggleTerminalSplit", utils.toggle_terminal_sp
 
 --------------------------------------------------------------------------------
 -- Key bindings/ mappings
+--------------------------------------------------------------------------------
+-- Ex.
+-- vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = buffnr,
+--                                               desc = "vim.lsp.buf.hover",
+--                                               remap=false,
+--                                               silent=false,
+--                                               expr=false
+--                                             })
+-- vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
 --------------------------------------------------------------------------------
 local def_opt = { remap = false }
 local map = vim.keymap.set
