46fb8e5a0764d121bd1b9cf52a63ac1a5bb174f0
Hristo Gueorguiev <izo@hristo-work.local>
2025-02-18 16:59:45 -0500

    quick rebuild req. script

---
+++ hristo-work.local/rebuild_req.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+./mackeysinstaller.sh
+brew install zsh-syntax-highlighting
+brew install zsh-autocomplete
+brew install ripgrep
+brew install lsd
+brew install bat
+brew install yazi
+brew install neovim
+brew install kitty
+
