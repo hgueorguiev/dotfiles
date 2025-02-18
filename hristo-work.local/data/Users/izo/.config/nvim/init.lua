1635c21c1713df3b66e34f25aae6b02d9538aa11
Hristo Gueorguiev <izo@hristo-work.local>
2025-02-18 17:05:07 -0500

    lazy bootstarp, ignore history

--- hristo-work.local/data/Users/izo/.config/nvim/init.lua
+++ hristo-work.local/data/Users/izo/.config/nvim/init.lua
@@ -76,7 +76,32 @@ vim.api.nvim_create_user_command("ToggleTerminalSplit", utils.toggle_terminal_sp
 --------------------------------------------------------------------------------
 -- Plugin manager
 --------------------------------------------------------------------------------
-local lazypath = vim.fn.stdpath("data") .. "/site/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.env.LAZY_PATH and not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.api.nvim_echo({
+    {
+      "Cloning lazy.nvim\n\n",
+      "DiagnosticInfo",
+    },
+  }, true, {})
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local ok, out = pcall(vim.fn.system, {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    lazyrepo,
+    lazypath,
+  })
+  if not ok or vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim\n", "ErrorMsg" },
+      { vim.trim(out or ""), "WarningMsg" },
+      { "\nPress any key to exit...", "MoreMsg" },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
 vim.opt.rtp:prepend(lazypath)
 local plugins = {
 	---- List of plugins...
--- hristo-work.local/data/Users/izo/.zprofile
+++ hristo-work.local/data/Users/izo/.zprofile
@@ -4,6 +4,4 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 # SSH key management
 eval "$(ssh-agent -s)"
 
-# Config paths setup
-
 export ZDOTDIR="$HOME/.config/zsh" # Relocate zsh dot files homedir
