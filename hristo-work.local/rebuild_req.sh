8fe7be1d64f98ce5a4311296af1b64700d5b43d2
Hristo Gueorguiev <izo@hristo-work.local>
2025-02-25 12:12:41 -0500

    Install req. updates

--- hristo-work.local/rebuild_req.sh
+++ hristo-work.local/rebuild_req.sh
@@ -3,11 +3,12 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 ./mackeysinstaller.sh
 brew install zsh-syntax-highlighting
-brew install zsh-autocomplete
+# brew install zsh-autocomplete - went to config `completion.zsh` added to .zshrc for now
 brew install ripgrep
+brew install fzf
 brew install lsd
 brew install bat
 brew install yazi
 brew install neovim
 brew install kitty
-
+brew install git-delta
